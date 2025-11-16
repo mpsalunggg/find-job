15d741e46eb93c926bff2952eafefe4fa5cdc9c2
mpsalunggg <putrasatria893@gmail.com>
2025-11-16 23:48:54 +0700

    chore: adjust new wip rules commit

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,5 +1,10 @@
+import defaultConfig from "@commitlint/config-conventional";
+
 const commitlintConfig = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", [...defaultConfig.rules["type-enum"][2], "wip"]],
+  },
 };
 
 export default commitlintConfig;
