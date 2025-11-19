88f4293d36d4439f11c7176df1e2874caab13ddc
mpsalunggg <putrasatria893@gmail.com>
2025-11-20 00:23:26 +0700

    feat: add redirect back to home

--- src/features/applicant/pages/ApplyPage.tsx
+++ src/features/applicant/pages/ApplyPage.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { useParams } from "next/navigation";
 import FormApply from "../components/FormApply";
 import { useGetListFormFields } from "../applicant.hooks";
 import { Skeleton } from "@/components/ui/skeleton";
+import Link from "next/link";
 
 export default function ApplyPage() {
   const params = useParams();
@@ -22,13 +23,15 @@ export default function ApplyPage() {
           <Card className="relative h-[90vh] gap-0 overflow-hidden rounded-none border border-none border-neutral-200 bg-white p-0 shadow-none">
             <CardContent className="no-scrollbar relative m-0 h-full overflow-y-auto px-0">
               <div className="flex gap-2 border-x border-t px-4 pt-4 pb-6 lg:gap-4 lg:px-10 lg:pt-10">
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="border-neutral h-7 w-7 border-2 p-1"
-                >
-                  <ArrowLeftIcon strokeWidth={4} />
-                </Button>
+                <Link href="/">
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="border-neutral h-7 w-7 border-2 p-1"
+                  >
+                    <ArrowLeftIcon strokeWidth={4} />
+                  </Button>
+                </Link>
                 <p className="text-lg font-bold">Apply Front End at Rakamin</p>
               </div>
               <FormApply
